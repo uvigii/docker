1db21f93c00ae8a83928b486d2c3edbbef1e2e0f
Milan Enev <uvigii@mail.bg>
2019-01-18 08:57:17 +0200

    git module remove

--- ns5_ub18/scripts/install_packages.sh
+++ ns5_ub18/scripts/install_packages.sh
@@ -3,14 +3,13 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update  -yq
 apt-get install -yq --no-install-recommends apt-utils
 apt-get dist-upgrade -yq
-git submodule update --init --recursive
 wget -q http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.gz
 tar -xzf node-v${NODE_VERSION}-linux-x64.tar.gz
 mv node-v${NODE_VERSION}-linux-x64 /opt/node
 rm node-v${NODE_VERSION}-linux-x64.tar.gz
 PATH=${PATH}:/opt/node/bin
-npm install -g nativescript --unsafe-perm
-npm install nativescript --unsafe-perm
+npm install -g nativescript
+npm install nativescript
 $ANDROID_HOME/tools/bin/sdkmanager "tools" "platform-tools" "platforms;android-26" "build-tools;26.0.2" "extras;android;m2repository" "extras;google;m2repository" > /dev/null
 npm i
 
