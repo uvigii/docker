f11c9765052a381b956ca49ec1bbd913890a3f56
Milan Enev <uvigii@mail.bg>
2019-01-18 23:56:50 +0200

    Update install_packages.sh

--- ns5_ub18/scripts/install_packages.sh
+++ ns5_ub18/scripts/install_packages.sh
@@ -7,12 +7,13 @@ apt-get dist-upgrade -yq
 useradd -ms /bin/bash nativescript
 
 # Utilities
-apt-get update
-apt-get -y install apt-transport-https unzip git curl usbutils --no-install-recommends
+apt-get -yq install apt-transport-https unzip git curl usbutils gnupg --no-install-recommends
 
 # JAVA
-apt-get update
-apt-get -y install default-jdk --no-install-recommends
+apt-get -yq install default-jdk --no-install-recommends
+
+# Android build requirements
+apt-get -y install lib32stdc++6 lib32z1 --no-install-recommends
 
 # NodeJS
 curl -sL https://deb.nodesource.com/setup_10.x | bash - 
@@ -23,8 +24,7 @@ apt-get -y install nodejs --no-install-recommends
 npm install -g nativescript
 tns error-reporting disable
 
-# Android build requirements
-apt-get -y install lib32stdc++6 lib32z1 --no-install-recommends
+
 
 # Android SDK
 $ANDROID_HOME/tools/bin/sdkmanager "tools" "platform-tools" "platforms;android-26" "build-tools;26.0.2" "extras;android;m2repository" "extras;google;m2repository" > /dev/null
