3e99dcc7f789629e08b1aa2df5aef8adeafd9f56
Milan Enev <uvigii@mail.bg>
2019-01-18 09:27:30 +0200

    Update install_packages.sh

--- ns5_ub18/scripts/install_packages.sh
+++ ns5_ub18/scripts/install_packages.sh
@@ -8,8 +8,8 @@ tar -xzf node-v${NODE_VERSION}-linux-x64.tar.gz
 mv node-v${NODE_VERSION}-linux-x64 /opt/node
 rm node-v${NODE_VERSION}-linux-x64.tar.gz
 PATH=${PATH}:/opt/node/bin
-npm install -g nativescript
-npm install nativescript
+npm install -g nativescript --unsafe-perm
+npm install nativescript --unsafe-perm
 $ANDROID_HOME/tools/bin/sdkmanager "tools" "platform-tools" "platforms;android-26" "build-tools;26.0.2" "extras;android;m2repository" "extras;google;m2repository" > /dev/null
 npm i
 
