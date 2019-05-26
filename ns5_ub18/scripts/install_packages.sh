18ab024e349a1d228083dc9c313fb81f9b574680
Milan Enev <uvigii@mail.bg>
2019-05-26 15:34:48 +0300

    Update install_packages.sh

--- ns5_ub18/scripts/install_packages.sh
+++ ns5_ub18/scripts/install_packages.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Init
 export DEBIAN_FRONTEND=noninteractive
+export ANDROID_HOME="/opt/android-sdk"
+export SDK_URL="https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip"
+export GRADLE_URL="https://services.gradle.org/distributions/gradle-5.4.1-bin.zip"
+export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
 apt-get update -yq
 apt-get install -yq --no-install-recommends apt-utils
 apt-get dist-upgrade -yq
@@ -13,7 +18,23 @@ apt-get -yq install apt-transport-https unzip git curl usbutils gnupg --no-insta
 apt-get -yq install default-jdk --no-install-recommends
 
 # Android build requirements
-apt-get -y install lib32stdc++6 lib32z1 --no-install-recommends
+apt-get -y install lib32stdc++6 lib32ncurses5 lib32z1 --no-install-recommends
+
+# Android SDK
+mkdir -p "$ANDROID_HOME" .android
+cd "$ANDROID_HOME" 
+curl -o sdk.zip $SDK_URL \
+ && unzip sdk.zip \
+ && rm sdk.zip \
+ && yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+
+wget $GRADLE_URL -O gradle.zip \
+ && unzip gradle.zip \
+ && mv gradle-4.5.1 gradle \
+ && rm gradle.zip \
+ && mkdir .gradle
+
+$ANDROID_HOME/tools/bin/sdkmanager "tools" "emulator" "platform-tools" "platforms;android-28" "build-tools;28.0.3" "extras;android;m2repository" "extras;google;m2repository"
 
 # NodeJS
 curl -sL https://deb.nodesource.com/setup_10.x | bash - 
@@ -23,11 +44,6 @@ apt-get -yq install nodejs --no-install-recommends
 # NativeScript
 npm install -g nativescript --unsafe-perm
 tns error-reporting disable
-
-# Android SDK
-export ANDROID_HOME=/opt/android-sdk
-export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
-$ANDROID_HOME/tools/bin/sdkmanager "tools" "platform-tools" "platforms;android-26" "build-tools;26.0.2" "extras;android;m2repository" "extras;google;m2repository" > /dev/null
 
 apt-get clean
 rm -rf /var/lib/apt/lists/*
