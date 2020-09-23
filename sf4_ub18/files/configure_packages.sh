d7757600a4dd6cd59d37994e7e1bd4deef2e2f48
Milan Enev <milan@mreji.net>
2020-09-23 23:44:14 +0300

    Mod: moved to Ubuntu 20.04

--- sf4_ub18/files/configure_packages.sh
+++ sf4_ub18/files/configure_packages.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 ln -snf /usr/share/zoneinfo/UTC /etc/localtime || exit
 echo "UTC" > /etc/timezone || exit
-echo "date.timezone = UTC" >> /etc/php/7.2/cli/conf.d/php.ini || exit
-echo "date.timezone = UTC" >> /etc/php/7.2/fpm/conf.d/php.ini || exit	
-cp -f /tmp/php-fpm.conf /etc/php/7.2/fpm/					  || exit
-cp -f /tmp/php-fpm.www.conf 	/etc/php/7.2/fpm/pool.d/www.conf	  || exit
-touch /etc/php/7.2/fpm/99-xdebug.ini        || exit                          
-chmod o+w /etc/php/7.2/fpm/99-xdebug.ini    || exit
+echo "date.timezone = UTC" >> /etc/php/7.4/cli/conf.d/php.ini || exit
+echo "date.timezone = UTC" >> /etc/php/7.4/fpm/conf.d/php.ini || exit	
+cp -f /tmp/php.ini /etc/php/7.4/fpm/					  || exit
+cp -f /tmp/php-fpm.conf /etc/php/7.4/fpm/					  || exit
+cp -f /tmp/php-fpm.www.conf 	/etc/php/7.4/fpm/pool.d/www.conf	  || exit
+touch /etc/php/7.4/fpm/99-xdebug.ini        || exit                          
+chmod o+w /etc/php/7.4/fpm/99-xdebug.ini    || exit
 #
 mkdir -p /var/www/symfony                   || exit
 chown www-data:www-data /var/www/symfony    || exit
@@ -15,5 +16,3 @@ cp -f /tmp/nginx.default.conf /etc/nginx/sites-available/default || exit
 cp -f /tmp/nginx.conf /etc/nginx/           || exit
 #
 chmod +x /tmp/start.sh                      || exit
-
-
--- sf4_ub18/files/start.sh
+++ sf4_ub18/files/start.sh
@@ -4,14 +4,14 @@ if [ -z ${APP_ENV+x} ]; then echo "No APP_ENV"; export APP_ENV="test"; fi
 echo "Env "$APP_ENV
 if [ $APP_ENV = "dev" ]; then
     echo "."
-    echo "zend_extension=xdebug.so"  >  /etc/php/7.2/fpm/99-xdebug.ini
-    echo "[Xdebug]"                  >> /etc/php/7.2/fpm/99-xdebug.ini
-    echo "xdebug.remote_enable=true" >> /etc/php/7.2/fpm/99-xdebug.ini
-    echo "xdebug.remote_port=5902"   >> /etc/php/7.2/fpm/99-xdebug.ini
+    echo "zend_extension=xdebug.so"  >  /etc/php/7.4/fpm/99-xdebug.ini
+    echo "[Xdebug]"                  >> /etc/php/7.4/fpm/99-xdebug.ini
+    echo "xdebug.remote_enable=true" >> /etc/php/7.4/fpm/99-xdebug.ini
+    echo "xdebug.remote_port=5902"   >> /etc/php/7.4/fpm/99-xdebug.ini
     #echo "xdebug.default_enable=1"   >>
     #echo "xdebug.remote_autostart = 1"
     #echo "xdebug.remote_handler = dbgp" >>
-    echo "xdebug.remote_host=localhost"  >> /etc/php/7.2/fpm/99-xdebug.ini   
+    echo "xdebug.remote_host=localhost"  >> /etc/php/7.4/fpm/99-xdebug.ini   
 elif [ $APP_ENV = "test" ]; then
     echo "."
 else
@@ -21,4 +21,4 @@ fi
 echo "Starting Nginx ..."
 /usr/sbin/nginx
 echo "Starting FPM ..."
-/usr/sbin/php-fpm7.2 -F
+/usr/sbin/php-fpm7.4 -F
